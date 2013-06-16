7b84c718c795e99ca9f48deb137292c1badf2da3
Sano Fužir <sanofuzir@Sanos-MacBook-Pro.local>
2013-06-16 22:13:54 +0200

    implementing FOSuserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new sano\CoreBundle\CoreBundle(),
             new sano\StaticBundle\StaticBundle(),
             new sano\AdminBundle\AdminBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new sano\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/sano/UserBundle/UserBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace sano\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle
+{
+    public function getParent()
+    {
+            return 'FOSUserBundle';
+    }
+}
